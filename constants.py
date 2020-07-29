e9a333e13d3a8b3fedb80edc2140bf399eb05947
Nhan Ha <nhantrungha@gmail.com>
2020-07-28 19:26:32 -0700

    added new files

---
+++ constants.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+#Game Sizes
+GAME_WIDTH = 800
+GAME_HEIGHT = 600
+
+#color definitions
+COLOR_BLACK = (0, 0, 0)
+COLOR_BLACK = (255, 255, 255)
+COLOR_GREY = (100, 100, 100)
+
+#Game colors
+COLOR_DEFAULT_BG = COLOR_GREY\
+
+#SPRITES
+S_PLAYER = pygame.image.load("data/snake.png")
--- main_game.py
+++ main_game.py
@@ -1,4 +1,52 @@
 import pygame
 import tcod as libtcod
 
-def game_main_loop():as
+# game files
+import constants
+
+def draw_game():
+
+    global SURFACE_MAIN
+
+    #TODO clear the surface
+    SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
+    #TODO draw the map
+
+    #TODO draw the character
+    SURFACE_MAIN.blit(constants.S_PLAYER, (200, 200))
+    #TODO update the display
+    pygame.display.flip()
+
+def game_main_loop():
+    """In this function, we loop the main game"""
+    game_quit = False
+
+    while not game_quit:
+        # TODO get player input
+        events_list = pygame.event.get()
+        # TODO process input
+        for event in events_list:
+            if event.type == pygame.QUIT:
+                game_quit = True
+
+        # TODO draw the game
+        draw_game()
+    # TODO quit the game
+    pygame.quit()
+    exit()
+
+
+def game_initialize():
+    """This function initializes the main window, and pygame"""
+
+    global SURFACE_MAIN
+
+    # initialize pygame
+    pygame.init()
+
+    SURFACE_MAIN = pygame.display.set_mode((constants.GAME_WIDTH, constants.GAME_HEIGHT))
+
+
+if __name__ == '__main__':
+    game_initialize()
+    game_main_loop()
