4ba5068d9ec57be7bde2c74110bcd78072a56ae4
Nhan Ha <nhantrungha@gmail.com>
2020-07-29 19:22:06 -0700

    added map functionality

--- constants.py
+++ constants.py
@@ -2,17 +2,25 @@ import pygame
 
 pygame.init()
 
-#Game Sizes
+# Game Sizes
 GAME_WIDTH = 800
 GAME_HEIGHT = 600
+CELL_WIDTH = 32
+CELL_HEIGHT = 32
 
-#color definitions
+# MAP VARS
+MAP_WIDTH = 30
+MAP_HEIGHT = 30
+
+# color definitions
 COLOR_BLACK = (0, 0, 0)
-COLOR_BLACK = (255, 255, 255)
+COLOR_WHITE = (255, 255, 255)
 COLOR_GREY = (100, 100, 100)
 
-#Game colors
-COLOR_DEFAULT_BG = COLOR_GREY\
+# Game colors
+COLOR_DEFAULT_BG = COLOR_GREY
 
-#SPRITES
+# SPRITES
 S_PLAYER = pygame.image.load("data/snake.png")
+S_WALL = pygame.image.load("data/wall.png")
+S_FLOOR = pygame.image.load("data/floor.png")
--- main_game.py
+++ main_game.py
@@ -4,18 +4,73 @@ import tcod as libtcod
 # game files
 import constants
 
-def draw_game():
 
+#      _______.___________..______       __    __    ______ .___________.
+#     /       |           ||   _  \     |  |  |  |  /      ||           |
+#    |   (----`---|  |----`|  |_)  |    |  |  |  | |  ,----'`---|  |----`
+#     \   \       |  |     |      /     |  |  |  | |  |         |  |
+# .----)   |      |  |     |  |\  \----.|  `--'  | |  `----.    |  |
+# |_______/       |__|     | _| `._____| \______/   \______|    |__|
+
+class struc_Tile:
+    def __init__(self, block_path):
+        self.block_path = block_path
+
+
+# .___  ___.      ___      .______
+# |   \/   |     /   \     |   _  \
+# |  \  /  |    /  ^  \    |  |_)  |
+# |  |\/|  |   /  /_\  \   |   ___/
+# |  |  |  |  /  _____  \  |  |
+# |__|  |__| /__/     \__\ | _|
+
+def map_create():
+    new_map = [[struc_Tile(False) for y in range(0, constants.MAP_HEIGHT)] for x in range(0, constants.MAP_WIDTH)]
+
+    new_map[10][10].block_path = True
+    new_map[10][15].block_path = True
+
+    return new_map
+
+#  _______  .______          ___   ____    __    ____  __  .__   __.   _______
+# |       \ |   _  \        /   \  \   \  /  \  /   / |  | |  \ |  |  /  _____|
+# |  .--.  ||  |_)  |      /  ^  \  \   \/    \/   /  |  | |   \|  | |  |  __
+# |  |  |  ||      /      /  /_\  \  \            /   |  | |  . `  | |  | |_ |
+# |  '--'  ||  |\  \----./  _____  \  \    /\    /    |  | |  |\   | |  |__| |
+# |_______/ | _| `._____/__/     \__\  \__/  \__/     |__| |__| \__|  \______|
+
+def draw_game():
     global SURFACE_MAIN
 
-    #TODO clear the surface
+    # TODO clear the surface
     SURFACE_MAIN.fill(constants.COLOR_DEFAULT_BG)
-    #TODO draw the map
 
-    #TODO draw the character
+    # TODO draw the map
+    draw_map(GAME_MAP)
+
+    # TODO draw the character
     SURFACE_MAIN.blit(constants.S_PLAYER, (200, 200))
-    #TODO update the display
+
+    # TODO update the display
     pygame.display.flip()
+
+
+def draw_map(map_to_draw):
+    for x in range(0, constants.MAP_WIDTH):
+        for y in range(0, constants.MAP_HEIGHT):
+            if map_to_draw[x][y].block_path:
+                #draw wall
+                SURFACE_MAIN.blit(constants.S_WALL, (x * constants.CELL_WIDTH, y * constants.CELL_HEIGHT))
+            else:
+                SURFACE_MAIN.blit(constants.S_FLOOR, (x * constants.CELL_WIDTH, y * constants.CELL_HEIGHT))
+                #draw floor
+
+#   _______      ___      .___  ___.  _______
+#  /  _____|    /   \     |   \/   | |   ____|
+# |  |  __     /  ^  \    |  \  /  | |  |__
+# |  | |_ |   /  /_\  \   |  |\/|  | |   __|
+# |  |__| |  /  _____  \  |  |  |  | |  |____
+#  \______| /__/     \__\ |__|  |__| |_______|
 
 def game_main_loop():
     """In this function, we loop the main game"""
@@ -39,14 +94,16 @@ def game_main_loop():
 def game_initialize():
     """This function initializes the main window, and pygame"""
 
-    global SURFACE_MAIN
+    global SURFACE_MAIN, GAME_MAP
 
     # initialize pygame
     pygame.init()
 
     SURFACE_MAIN = pygame.display.set_mode((constants.GAME_WIDTH, constants.GAME_HEIGHT))
 
+    GAME_MAP = map_create()
 
 if __name__ == '__main__':
     game_initialize()
     game_main_loop()
+
