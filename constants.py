58dd5ae9d56b3b5d5203ce7381e353c189f8b89a
Nhan Ha <nhantrungha@gmail.com>
2020-08-03 19:31:14 -0700

    added basic attack and AI

--- constants.py
+++ constants.py
@@ -9,8 +9,8 @@ CELL_WIDTH = 32
 CELL_HEIGHT = 32
 
 # MAP VARS
-MAP_WIDTH = 30
-MAP_HEIGHT = 30
+MAP_WIDTH = 20
+MAP_HEIGHT = 20
 
 # color definitions
 COLOR_BLACK = (0, 0, 0)
--- main_game.py
+++ main_game.py
@@ -25,20 +25,40 @@ class struc_Tile:
 #  \______/  |______/   \______/  |_______| \______|    |__|    |_______/
 
 class obj_Actor:
-    def __init__(self, x, y, name_object, sprite, creature=None):
+    def __init__(self, x, y, name_object, sprite, creature=None, ai=None):
         self.x = x  # map address
         self.y = y
         self.sprite = sprite
 
+        self.creature = creature
         if creature:
-            self.creature = creature
             creature.owner = self
+
+        self.ai = ai
+        if ai:
+            ai.owner = self
 
     def draw(self):
         SURFACE_MAIN.blit(self.sprite, (self.x * constants.CELL_WIDTH, self.y * constants.CELL_HEIGHT))
 
     def move(self, dx, dy):
-        if not GAME_MAP[self.x + dx][self.y + dy].block_path:
+
+        tile_is_wall = (GAME_MAP[self.x + dx][self.y + dy].block_path == True)
+
+        target = None
+
+        for object in GAME_OBJECTS:
+            if (object is not self and
+                    object.x == self.x + dx and
+                    object.y == self.y + dy and
+                    object.creature):
+                target = object
+                break
+
+        if target:
+            print(self.creature.name_instance + " attacks " + target.creature.name_instance)
+
+        if not tile_is_wall and target is None:
             self.x += dx
             self.y += dy
 
@@ -59,9 +79,23 @@ class com_Creature:
         self.hp = hp
 
 
-# class com_Item:
+# TODO class com_Item
 
-# class com_Container:
+# TODO class com_Container
+
+#     _    ___
+#    / \  |_ _|
+#   / _ \  | |
+#  / ___ \ | |
+# /_/   \_\___|
+
+class ai_Test:
+    """
+    Once per turn, execute:
+    """
+
+    def take_turn(self):
+        self.owner.move(libtcod.random_get_int(0, -1, 1), libtcod.random_get_int(0, -1, 1))
 
 
 # .___  ___.      ___      .______
@@ -76,6 +110,13 @@ def map_create():
 
     new_map[10][10].block_path = True
     new_map[10][15].block_path = True
+
+    for x in range(constants.MAP_WIDTH):
+        new_map[x][0].block_path = True
+        new_map[x][constants.MAP_HEIGHT - 1].block_path = True
+    for y in range(constants.MAP_HEIGHT):
+        new_map[0][y].block_path = True
+        new_map[constants.MAP_WIDTH - 1][y].block_path = True
 
     return new_map
 
@@ -96,10 +137,9 @@ def draw_game():
     #  draw the map
     draw_map(GAME_MAP)
 
-    ENEMY.draw()
-
-    #  draw the character
-    PLAYER.draw()
+    #  draw all objects
+    for obj in GAME_OBJECTS:
+        obj.draw()
 
     #  update the display
     pygame.display.flip()
@@ -127,22 +167,20 @@ def game_main_loop():
     """In this function, we loop the main game"""
     game_quit = False
 
+    # player action definition
+    player_action = "no-action"
+
     while not game_quit:
-        #  get player input
-        events_list = pygame.event.get()
-        #  process input
-        for event in events_list:
-            if event.type == pygame.QUIT:
-                game_quit = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_k:
-                    PLAYER.move(0, -1)
-                if event.key == pygame.K_j:
-                    PLAYER.move(0, 1)
-                if event.key == pygame.K_h:
-                    PLAYER.move(-1, 0)
-                if event.key == pygame.K_l:
-                    PLAYER.move(1, 0)
+
+        # handle player input
+        player_action = game_handle_keys()
+
+        if player_action == "QUIT":
+            game_quit = True
+        elif player_action != "no-action":
+            for obj in GAME_OBJECTS:
+                if obj.ai:
+                    obj.ai.take_turn()
 
         #  draw the game
         draw_game()
@@ -154,20 +192,51 @@ def game_main_loop():
 def game_initialize():
     """This function initializes the main window, and pygame"""
 
-    global SURFACE_MAIN, GAME_MAP, PLAYER, ENEMY
+    global SURFACE_MAIN, GAME_MAP, PLAYER, ENEMY, GAME_OBJECTS
 
     # initialize pygame
     pygame.init()
 
-    SURFACE_MAIN = pygame.display.set_mode((constants.GAME_WIDTH, constants.GAME_HEIGHT))
+    SURFACE_MAIN = pygame.display.set_mode((constants.MAP_WIDTH * constants.CELL_WIDTH,
+                                            constants.MAP_HEIGHT * constants.CELL_HEIGHT))
 
     GAME_MAP = map_create()
 
     creature_com1 = com_Creature("greg")
-    PLAYER = obj_Actor(0, 0, "python", constants.S_PLAYER, creature=creature_com1)
+    PLAYER = obj_Actor(1, 1, "python", constants.S_PLAYER, creature=creature_com1)
 
     creature_com2 = com_Creature("jackie")
-    ENEMY = obj_Actor(15, 15, "crab", constants.S_ENEMY, creature=creature_com2)
+    ai_com = ai_Test()
+    ENEMY = obj_Actor(15, 15, "crab", constants.S_ENEMY, creature=creature_com2, ai=ai_com)
+
+    GAME_OBJECTS = [PLAYER, ENEMY]
+
+
+def game_handle_keys():
+    #  get player input
+    events_list = pygame.event.get()
+
+    #  process input
+    for event in events_list:
+        if event.type == pygame.QUIT:
+            return "QUIT"
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_k:
+                PLAYER.move(0, -1)
+                return "player-moved"
+
+            if event.key == pygame.K_j:
+                PLAYER.move(0, 1)
+                return "player-moved"
+
+            if event.key == pygame.K_h:
+                PLAYER.move(-1, 0)
+                return "player-moved"
+
+            if event.key == pygame.K_l:
+                PLAYER.move(1, 0)
+                return "player-moved"
+    return "no-action"
 
 
 if __name__ == '__main__':
